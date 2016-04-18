51feba573b1c1d967e9d7c375fc902431c4df20e
WUSTL STS <STS-MBP15@stsgrgco1504.dhcp.wustl.edu>
2016-04-18 18:31:58 -0500

    labwork

--- menu.js
+++ menu.js
@@ -1,15 +1,36 @@
 // begin with a blank/new order
-var order = '';
+var order = [];
 
+var burgers = ["None", "Double Double Burger","Cheeseburger","Hamburger", ];
+var fries  = ["None", "Regular Fries","Cheesy Fries", ];
+var shakes = ["None", "Chocolate Shake", "Strawberry Shake", "Vanilla Shake", "None"];
 
 // build a function to ask questions
 
-function question() {
-  // your code goes here
+function question(foodArray) {
+
+  var orderPrompt = "Choose an option. Please enter number: \n";
+  var answerPrompt = "notanumber";
+
+  for(i=0;i<foodArray.length;i++){
+    orderPrompt += i + " - " + foodArray[i] + "\n";
+  }
+
+  while(isNaN(answerPrompt)){
+    answerPrompt = prompt(orderPrompt);
+  }
+
+ if(answerPrompt != 0){
+   order.push(foodArray[answerPrompt]);
+  }
 }
 
-// call the QUESTION function with the order options
-question();
+question(burgers);
+question(fries);
+question(shakes);
 
 // output their order
-document.write("<p>You ordered: " + order + "<p>");
+
+for(j=0;j<order.length;j++){
+  document.write("<p>You ordered: " + order[j] + "<p>");
+}
